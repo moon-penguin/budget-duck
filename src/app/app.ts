3a082d03adf789b74a8fb9bd6b86066df374ce40
Nicat Achmedow <nidzad.achmedow@codecentric.de>
2024-10-02 11:11:05 +0200

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { FastifyInstance } from 'fastify';
+import AutoLoad from '@fastify/autoload';
+
+/* eslint-disable-next-line */
+export interface AppOptions {}
+
+export async function app(fastify: FastifyInstance, opts: AppOptions) {
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: { ...opts },
+  });
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: { ...opts },
+  });
+}
