c5d357cf8345b3f98bbb8c28a0bc629480060f71
Nicat Achmedow <nidzad.achmedow@codecentric.de>
2024-11-15 14:16:13 +0100

    add graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify';
 import { app } from './app/app';
 import applicationConfig from './app/configuration/application.config';
 import { pinoLogger } from './app/utils/logger.utils';
+import closeWithGrace from 'close-with-grace';
 
 const host = applicationConfig.host;
 const port = applicationConfig.port;
@@ -11,6 +12,15 @@ const server = Fastify({
 });
 
 server.register(app);
+
+closeWithGrace(async function ({ signal, err }) {
+  if (err) {
+    server.log.error({ err }, 'server closing with error');
+  } else {
+    server.log.info(`${signal} received, server closing`);
+  }
+  await server.close();
+});
 
 server.listen({ port, host }, (err) => {
   if (err) {
